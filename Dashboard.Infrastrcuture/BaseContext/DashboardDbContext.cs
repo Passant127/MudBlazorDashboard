dd23739884d57947426e289ccdf9c78307e0789f
user <user@PASSANT-MOHAMED>
2024-08-06 17:06:43 +0200

    add rest project

---
+++ Dashboard.Infrastrcuture/BaseContext/DashboardDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Dashboard.Infrastrcuture.BaseContext;
+
+public class DashboardDbContext : DbContext
+{
+    public DashboardDbContext(DbContextOptions dbContext) : base(dbContext)
+    {
+        
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseLazyLoadingProxies();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+ 
+
+}
