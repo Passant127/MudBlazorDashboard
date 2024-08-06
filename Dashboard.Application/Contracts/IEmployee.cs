7d03aad5722d7d46df9e13fb60c9b1cd572680b1
user <user@PASSANT-MOHAMED>
2024-08-06 17:18:43 +0200

    add employee contract

---
+++ Dashboard.Application/Contracts/IEmployee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Application.Contracts;
+
+/// <summary>
+/// Provides an interface for employee-related services that manages employee data across the application. Inherits from IApplicationService and IScopedService.
+/// </summary>
+public interface IEmployeeService 
+{
+    /// <summary>
+    /// Asynchronously adds a new employee.
+    /// </summary>
+    /// <param name="employeeRequestDto">The data transfer object containing employee information.</param>
+    /// <returns>A task representing the result of adding the employee successfully.</returns>
+    public Task<Result> AddDepartmentAsync(DepartmentRequestDto employeeRequestDto);
+
+    /// <summary>
+    /// Asynchronously retrieves all employees.
+    /// </summary>
+    /// <returns>A task representing the result of retrieving a list of employee response data transfer objects.</returns>
+    public Task<Result<PaginationResult<DepartmentResponseDto>>> GetAllDepartmentsAsync(int itemCount, int index);
+
+    /// <summary>
+    /// Asynchronously retrieves a employee by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the employee to retrieve.</param>
+    /// <returns>A task representing the result of retrieving the employee response data transfer object.</returns>
+    public Task<Result<DepartmentResponseDto>> GetDepartmentByIdAsync(int id);
+
+    /// <summary>
+    /// Asynchronously updates a employee by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the employee to update.</param>
+    /// <param name="employeeRequestDto">The data transfer object containing updated employee information.</param>
+    /// <returns>A task representing the result of updating the employee response data transfer object.</returns>
+    public Task<Result<DepartmentResponseDto>> UpdateDepartmentAsync(int id, DepartmentRequestDto employeeRequestDto);
+
+    /// <summary>
+    /// Asynchronously deletes a employee by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the employee to delete.</param>
+    /// <returns>A task representing the result of deleting the employee successfully.</returns>
+    public Task<Result> DeleteDepartmentAsync(int id);
+
+    /// <summary>
+    /// Asynchronously searches for employees by name.
+    /// </summary>
+    /// <param name="text">The name of the employee to search for.</param>
+    /// <returns>A task representing the result of retrieving a list of employee response data transfer objects that match the search criteria.</returns>
+    public Task<Result<PaginationResult<DepartmentResponseDto>>> SearchDepartmentByTextAsync(string text, int itemCount, int index);
+}
